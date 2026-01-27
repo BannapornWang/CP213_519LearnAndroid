752897d5ad41f897581ed3aea7356decacd93913
Bannaporn <bannaporn.wangchuchob@g.swu.ac.th>
2026-01-27 16:01:16 +0700

    update

--- Project/lab/app/src/main/java/com/example/a519lablearnandroid/MainActivity2.kt
+++ Project/lab/app/src/main/java/com/example/a519lablearnandroid/MainActivity2.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia.DefaultTab.AlbumsTab.value
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a519lablearnandroid.ui.theme._519LabLearnAndroidTheme
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,11 +46,31 @@ class MainActivity2 : ComponentActivity() {
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    var inputText by remember { mutableStateOf("") } //mutable...("")
+    Column {
+            Text(
+            text = "Hello $name! say ="+inputText,
+            modifier = modifier
+        )
+            Text(
+                text = "Good bye $name!",
+                modifier = modifier
+            )
+//            TextField(
+//                value = inputText,
+//                onValueChange = {
+//                    inputText = it
+//                }
+//            )
+        TextField(
+            value = inputText,
+            onValueChange = {
+                inputText = it
+            }
+        )
+        }
+    }
+
 
 @Preview(showBackground = true)
 @Composable
